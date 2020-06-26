954b6f4e8889feacde8d1684e7cbd3fae9f3f8c6
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-06-26 07:56:35 +0545

    fix left slider

--- leapfrog/js/image-crousel/script.js
+++ leapfrog/js/image-crousel/script.js
@@ -1,70 +1,131 @@
 
-var sliderImages = document.querySelectorAll('.slide');
 
-var currentIndex = 0;
-var LIMIT = 400;
-var end = 0;
-imageWrapper = document.querySelector('.carousel-image-wrapper');
 
-document.getElementById('first').style.left = "-400px";
+/**
+ * @param {Number} LIMIT The width limit of carousel
+ * @param {Number} currentIndex The current index of carousel
+ * @param {function} slideRight To slide the image to right
+ * @param {function} slideLeft To slide the image to left
+ * @param {animator} imageSliderLeft To slide image to left on certain interval
+ * @param {animator} imageSliderRight To slide image to right on certain interval 
+ */
 
-function slideRight(){
-    currentIndex++;
-    if (currentIndex < sliderImages.length){
-        window.requestAnimationFrame(imageSliderRight);
-    } else if (currentIndex == sliderImages.length){
-        currentIndex = 1;
-        imageWrapper.style.left = "0px";
-        imageSliderRight();
+class Carousel {
+
+    constructor (LIMIT, sliderImages, imageWrapper){
+        this.slideRight = this.slideRight.bind(this);
+        this.slideLeft = this.slideLeft.bind(this);
+        this.imageSliderRight = this.imageSliderRight.bind(this);
+        this.imageSliderLeft = this.imageSliderLeft.bind(this);
+        this.currentIndex = 0;
+        this.LIMIT = LIMIT;
+        this.leftswap = 0;
+        this.sliderImages = sliderImages;
+        this.imageWrapper = imageWrapper
+        
 
     }
+
+    slideRight() {
+        this.currentIndex++;
+        if (this.currentIndex < this.sliderImages.length) {
+            window.requestAnimationFrame(this.imageSliderRight);
+        } else if (this.currentIndex == this.sliderImages.length) {
+            this.currentIndex = 1;
+            this.imageWrapper.style.left = "0px";
+            this.imageSliderRight();
+
+        }
+    }
+
+
+    slideLeft() {
+        this.currentIndex--;
+
+        if (this.currentIndex >= 0) {
+            window.requestAnimationFrame(this.imageSliderLeft);
+        } else if (this.currentIndex < 0) {
+            this.currentIndex = this.sliderImages.length - 1;
+            this.leftswap = 1;
+            this.imageWrapper.style.left = "-1200px";
+            this.imageSliderLeft();
+
+        }
+
+
+    }
+
+    imageSliderRight() {
+
+
+        var leftPx = parseInt(window.getComputedStyle(this.imageWrapper).left) - 10;
+        this.imageWrapper.style.left = leftPx + 'px';
+
+
+        if (parseInt(leftPx) != -(this.LIMIT * this.currentIndex)) {
+            window.requestAnimationFrame(this.imageSliderRight);
+        }
+
+        if (this.imageWrapper.style.left == "-400px") {
+            document.querySelector('#radio1').checked = true;
+
+        } else if (this.imageWrapper.style.left == "-800px") {
+            document.querySelector('#radio2').checked = true;
+
+        } else if (this.imageWrapper.style.left == "-1200px") {
+            document.querySelector('#radio3').checked = true;
+        } else if (this.imageWrapper.style.left == "-1600px"){
+            document.querySelector('#radio0').checked = true;
+        }
+    }
+
+
+    imageSliderLeft() {
+
+        if (this.imageWrapper.style.left == "0px" || this.imageWrapper.style.left == "-10px") {
+            document.querySelector('#radio0').checked = true;
+
+        } else if (this.imageWrapper.style.left == "-400px" || this.imageWrapper.style.left == "-410px") {
+            document.querySelector('#radio1').checked = true;
+
+        } else if (this.imageWrapper.style.left == "-1200px") {
+            document.querySelector('#radio3').checked = true;
+        } else if (this.imageWrapper.style.left == "-800px" || this.imageWrapper.style.left == "-810px") {
+            document.querySelector('#radio2').checked = true;
+        } else if (this.imageWrapper.style.left == "-1600px"){
+            document.querySelector('#radio3').checked = true;
+        }
+
+
+
+        if (this.leftswap != 1) {
+            var rightPx = parseInt(window.getComputedStyle(this.imageWrapper).left) + 10;
+            this.imageWrapper.style.left = rightPx + 'px';
+            if (parseInt(rightPx) != -(this.LIMIT * this.currentIndex)) {
+                window.requestAnimationFrame(this.imageSliderLeft);
+            }
+        } else {
+            this.leftswap = 0;
+            this.currentIndex = this.sliderImages.length - 2;
+        }
+    }
+
 }
 
 
-function slideLeft(){
-    currentIndex--;
-
-    if (currentIndex >= 0){
-        window.requestAnimationFrame(imageSliderLeft);
-    } else if (currentIndex < 0 ){
-        currentIndex = sliderImages.length - 1;
-        imageWrapper.style.left = "-400px";
-
-    }
-
-
-}
-
-function imageSliderRight(){
-    leftPx = parseInt(window.getComputedStyle(imageWrapper).left) - 10 ;
-    imageWrapper.style.left = leftPx + 'px';
-    
-    if (parseInt(leftPx) != -(LIMIT*currentIndex)){ 
-            window.requestAnimationFrame(imageSliderRight);
-    }
-} 
-
-
-function imageSliderLeft(){
-    rightPx = parseInt(window.getComputedStyle(imageWrapper).left) + 10 ;
-    imageWrapper.style.left = rightPx + 'px';
-    
-    if (parseInt(rightPx) != -(LIMIT*currentIndex) ){
-        window.requestAnimationFrame(imageSliderLeft);
-    }
-}
-
-if (currentIndex == 0 ){
+if (window.getComputedStyle(document.querySelector('.carousel-image-wrapper')).left == "0px") {
     document.querySelector('#radio0').checked = true;
-} 
-
-
-document.getElementById('next').onclick = function(){
-    console.log(currentIndex);
-    slideRight();
 }
 
-document.getElementById('previous').onclick = function(){
-    slideLeft();
+
+
+carousel = new Carousel(400, document.querySelectorAll('.slide'), document.querySelector('.carousel-image-wrapper')) //Creation of New carousel
+document.getElementById('next').onclick = function () {
+    carousel.slideRight();
 }
+document.getElementById('previous').onclick = function () {
+    carousel.slideLeft();
+}
+
+
 
