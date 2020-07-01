6595e25e962fb7712c0d9672d8c81a379460b0d6
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-07-01 10:41:57 +0545

    bullet implementation

--- leapfrog/js/car-racing/js/car.js
+++ leapfrog/js/car-racing/js/car.js
@@ -4,12 +4,13 @@ class Car {
         this.keys = {
             ArrowLeft: false,
             ArrowRight: false,
+            ArrowUp: false,
         }
         this.playerSpeed = 200;
         this.carLeft = 0;
         this.enemySpeed = 5;
         this.score = 0;
-
+        this.bulletCount = 10;
     }
 
     playerCar = () => {
@@ -42,14 +43,44 @@ class Car {
             top : parseInt(window.getComputedStyle(enemy).top),
         }
         var self = this;
+        let count = 0;
+
+
+        
 
         function move() {
-            let count = 0;
+
+            let healers = document.querySelectorAll('.healer');
+            if (healers){
+                healers.forEach(function(healer) {
+                    if (healer.top >= 700){
+                        healer.top = -(Math.random() *500) + 'px';
+                    }
+                    healer.style.top = healer.top + 5 + 'px';
+                    console.log(healer.style.top)
+                })
+            }
+
+            if (document.querySelector('.bullet')){
+                
+
+                var bullet = document.querySelector('.bullet');
+                if (parseInt(bullet.style.top) - parseInt(enemy.style.top) < 10 && parseInt(bullet.style.left) == parseInt(window.getComputedStyle(enemy).left)){
+                    console.log(parseInt(bullet.style.left))
+                    bullet.remove();
+                    console.log(count++);
+                    enemy.style.display = 'none';
+                    if( self.bulletCount < 5){
+                        self.initHealer();
+                    }
+                    // self.vehicleDestroy = self.vehicleDestroy + 1;
+                }
+            }
 
             isCollide(car, enemy);
             if (enemyPosition.top >= 700){
             if (document.querySelector('.play-again').style.display != 'inline-block'){
-
+                enemy.style.display = 'inline-block';
                 self.score = self.score + 3;
                 enemyPosition.top = - (Math.random()+n) * 500;
                 document.getElementById('score').innerHTML = self.score;
@@ -86,6 +117,8 @@ class Car {
         } else if (this.keys.ArrowRight && this.carLeft < 500) {
             this.carLeft += this.playerSpeed;
             this.gamePlay();
+        } else if (this.keys.ArrowUp){
+            this.bullet();
         }
     }
 
@@ -95,6 +128,37 @@ class Car {
         let road = playGround.getBoundingClientRect();
         playerCar.style.left = this.carLeft + 'px';
 
+    }
+
+    initHealer = () => {
+        const leftPosition = [100, 300, 500];
+        let healer = document.createElement('div');
+        healer.setAttribute('class', 'healer');
+        healer.top = Math.random() * 700;
+        const randomLeft = leftPosition[Math.floor(Math.random() * leftPosition.length)];
+        healer.style.left = randomLeft + 'px';
+        healer.style.top = healer.top + 'px';
+        document.querySelector('.playground').appendChild(healer);
+    }
+
+    bullet = () => {
+       
+        if (this.bulletCount > 0){
+            console.log("call")
+            this.bulletCount-- ;
+            document.getElementById('vehicleDestroy').innerHTML = this.bulletCount;
+            let bullet = document.createElement('div');
+            bullet.setAttribute('class', `bullet`);
+            bullet.top = 400 ;
+            bullet.style.left = document.querySelector('.playerCar').style.left;
+            bullet.style.top = bullet.top + 'px';
+            document.querySelector('.playground').appendChild(bullet);
+        }
+        
+    }
+
+    increaseBullet = () => {
+        this.bulletCount += 3;
     }
 
 
